daef61a49e30f56b238f8b3c8d9081d4fc078804
basilhs657 <basihstzimpraku123@gmail.com>
2023-11-22 17:46:10 +0200

    2

---
+++ exercise1.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+class Person
+{
+private:
+    /* data */
+public:
+    Person(/* args */);
+    ~Person();
+};
+
+Person::Person(/* args */)
+{
+}
+
+Person::~Person()
+{
+}
+
+
+class Secretary
+{
+private:
+    /* data */
+public:
+    Secretary(/* args */);
+    ~Secretary();
+};
+
+Secretary::Secretary(/* args */)
+{
+}
+
+Secretary::~Secretary()
+{
+}
+
+
+int main(){
+
+}
