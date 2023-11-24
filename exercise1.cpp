52c51bed866f3e630f3cf45fe2af7f051cbb2752
Antonis Sfikakis <AntonisSfikakis@gmail.com>
2023-11-24 14:37:00 +0200

    1,2,3,4

--- exercise1.cpp
+++ exercise1.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include<ostream>
 #include<istream>
+#include<map>
 #include <string>
 // athliopt mono
 using namespace std;
@@ -45,9 +46,9 @@ public:
 		ins  >> det.name;
 		cout << "give last Name:" << endl;
 		ins  >>  det.second_name; 
-		cout << "give Am" << endl;
+		cout << "give AM :" << endl;
 		ins >> det.AM;
-		cout << "give AN" << endl;
+		cout << "give AN :" << endl;
 		ins >> det.AN;
 		return ins;
 		//
@@ -64,33 +65,70 @@ public:
 
 int Person::person_counter = 0;
 
-// class Secretary
-// {
-// private:
-//     /* data */
-// public:
-//     Secretary(/* args */);
-//     ~Secretary();
-// };
+class Secretary
+{
+private:
+	map<long int, Person *> person_map ; // we gonna use map key:AM(aritmos mitrwou) value:person object
+public:
+	Secretary(){}; //empty constructor
+	Secretary& operator+(Person * other){
+		Person * pr = new Person(*other) ;  // dynamic allocation 
+		person_map[pr->GetAM()]=pr ; //add the person to the map 
+		return *this ;//return the objects pointer
+	}
+	friend istream & operator>>(istream & input ,Secretary& sec){
+		Person person ; 
+		cin >> person ;
+		Person  *pointer_person = new Person(person) ;
+		sec=sec+pointer_person; //using the + operatot with the function above 
+		return input;
+	}
+	friend ostream& operator<<(ostream& output,Secretary& sec){
+		cout << endl;
+		for(auto itr =sec.person_map.begin() ; itr != sec.person_map.end() ; itr++){
+			output << itr->second;	 //using the Persons class << operartor
+			output << endl;
+			
+		}
+		return output
+	}
+	void ExistingAM(Person * person ){
+		if(person_map[person->GetAM()]==NULL) //the value of the map is a pointer 
+			cout<<" PERSON DOES NOT EXIST"<< endl;
+		else
+		{
+			cout << "PERSON EXISTS" << endl;
+			cout << person ;
+		}	
+	
+	}
+	~Secretary(){
+		//memory dealocation 
+		for(auto it=person_map.begin(); it !=person_map.end() ; it++)
+			delete it->second;
+	}
+};
 
-// Secretary::Secretary(/* args */)
-// {
-// }
 
-// Secretary::~Secretary()
-// {
-// }
 
 int main(){
-	Person p(11112233 , "bill" , "sikakis" , "albanian");
-	//test
-	cout << "name is: " << p.GetFirstName() << endl << "surname: " << p.GetLastName() << endl << "AM: " << p.GetAM() << endl;
-    p.Person_counter();
+// 	Person p(11112233 , "bill" , "sikakis" , "albanian");
+// 	//test
+// 	cout << "name is: " << p.GetFirstName() << endl << "surname: " << p.GetLastName() << endl << "AM: " << p.GetAM() << endl;
+//     p.Person_counter();
 	
-//use of operation overload functions  (istream and ostream)
-	Person p1 ;
+// //use of operation overload functions  (istream and ostream)
+// 	Person p1 ;
 	
-	cin >> p1; 
-	cout << p1;
-    return 0;
+// 	cin >> p1; 
+// 	cout << p1;
+//     return 0;
+	Secretary sec;
+	cin >> sec ; 
+	cout <<sec;
+	Person  person ;
+	cin >> person;
+	cout << person ;
+	sec.ExistingAM(&person);
+	return 0;
 }
