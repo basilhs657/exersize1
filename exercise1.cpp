4ae05cb5784c35f092ea4c8ad79805b5e48a35c5
basilhs657 <basihstzimpraku123@gmail.com>
2023-11-27 16:21:28 +0200

    1111

--- exercise1.cpp
+++ exercise1.cpp
@@ -3,15 +3,18 @@
 #include<istream>
 #include<map>
 #include <string>
-// athliopt mono
+
 using namespace std;
+
+
 class Person
 {
+
 private:
 	long int AM; //arithmos mitrwoy
 	string second_name;//
-	string name	;//o
-	string AN; //arithmos astinomikhs taftot
+	string name	; // onoma
+	string AN; //arithmos astinomikhs taftotitas
 
 	static int person_counter;;//keeping track of object Persons created
     /* data */
@@ -21,27 +24,29 @@ public:
 		cout << "Person has been constructed" << endl;
 		person_counter++;
 	}; //initializer list
+	
 	Person(){}; //the constructor in to use the istream and ostream function 
 	void Person_counter(){
         cout << "The number of people created is: " << person_counter << endl;
     }
 	void SetAM(long int arithmos){ //imutators
-		AM=arithmos;
-	} 
-	void SetName(string first_name){
-		name=first_name;
-	}
+		AM=arithmos;					
+	} 			
+	void SetName(string first_name){	
+		name=first_name;			
+	}					
 	void SetSecondName(string last_name){
 		second_name=last_name;
 	}
 	void SetAN(string taftotita){
-			AN=taftotita;
+		AN=taftotita;
 	}
 	friend ostream & operator<<(ostream& par  ,const Person& pr) {
 		par << "Name: " << pr.name << endl << "Last Name: "<< pr.second_name << endl << "Arithmo mitro: " << pr.AM << endl << "Arithmo tautothtas: " << pr.AN << endl; 
 		return par;
 	}
 	friend istream & operator>>(istream& ins ,/*const not needed i think cause we change value of the pobject */ Person& det){
+		person_counter++; //because we create a new person without the constructor this means we should update the person counter
 		cout << "give Name" << endl;
 		ins  >> det.name;
 		cout << "give last Name:" << endl;
@@ -51,7 +56,6 @@ public:
 		cout << "give AN :" << endl;
 		ins >> det.AN;
 		return ins;
-		//
 	}
 
 	long int GetAM(){return AM;};
@@ -63,7 +67,6 @@ public:
 	}
 };
 
-int Person::person_counter = 0;
 
 class Secretary
 {
@@ -81,54 +84,75 @@ public:
 		cin >> person ;
 		Person  *pointer_person = new Person(person) ;
 		sec=sec+pointer_person; //using the + operatot with the function above 
+		delete pointer_person;
 		return input;
 	}
 	friend ostream& operator<<(ostream& output,Secretary& sec){
 		cout << endl;
 		for(auto itr =sec.person_map.begin() ; itr != sec.person_map.end() ; itr++){
-			output << itr->second;	 //using the Persons class << operartor
+			output << *itr->second;	 //using the Persons class << operartor
 			output << endl;
-			
 		}
-		return output
+		return output;
 	}
+
+	Secretary& operator=(const Secretary& sec){
+		
+		for(auto mp = person_map.begin(); mp != person_map.end(); mp++){
+			delete mp->second;
+		}
+		person_map.clear();
+		for(auto mp = sec.person_map.begin(); mp != sec.person_map.end(); mp++){
+
+			Person* cloned = new Person(*(mp->second)); 
+			person_map[mp->first] = cloned;
+		}
+		return *this;
+	}
+
+	Secretary(const Secretary& other):person_map(other.person_map){
+		for(auto mp = other.person_map.begin(); mp != other.person_map.end(); mp++){
+			Person* cloned = new Person(*(mp->second)); 
+			person_map[mp->first] = cloned;
+		}
+	}; // copy constructor 
+
 	void ExistingAM(Person * person ){
 		if(person_map[person->GetAM()]==NULL) //the value of the map is a pointer 
 			cout<<" PERSON DOES NOT EXIST"<< endl;
 		else
 		{
 			cout << "PERSON EXISTS" << endl;
-			cout << person ;
+			cout << *person; 
 		}	
 	
 	}
 	~Secretary(){
-		//memory dealocation 
-		for(auto it=person_map.begin(); it !=person_map.end() ; it++)
-			delete it->second;
+		for(auto pr = person_map.begin(); pr != person_map.end(); pr++){
+			delete pr->second;
+		}
+		person_map.clear();
+		cout << "sec has been destructed" << endl;
 	}
 };
 
+int Person::person_counter = 0;
 
 
 int main(){
-// 	Person p(11112233 , "bill" , "sikakis" , "albanian");
-// 	//test
-// 	cout << "name is: " << p.GetFirstName() << endl << "surname: " << p.GetLastName() << endl << "AM: " << p.GetAM() << endl;
-//     p.Person_counter();
 	
-// //use of operation overload functions  (istream and ostream)
-// 	Person p1 ;
+	Person p;
+	cin >> p;
+	p.Person_counter();
+	Secretary secretary;
+	cin >> secretary;
+	cin >> secretary;
+	cin >> secretary;
+	Person find;
+	cin >> find;
+	Secretary replace;
+	p.Person_counter();
 	
-// 	cin >> p1; 
-// 	cout << p1;
-//     return 0;
-	Secretary sec;
-	cin >> sec ; 
-	cout <<sec;
-	Person  person ;
-	cin >> person;
-	cout << person ;
-	sec.ExistingAM(&person);
-	return 0;
+
+
 }
