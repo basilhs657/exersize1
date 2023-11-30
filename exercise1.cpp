6ff9ef550d7fb60d5110025942973177cb3dfc29
basilhs657 <basihstzimpraku123@gmail.com>
2023-11-30 18:02:01 +0200

    finally

--- exercise1.cpp
+++ exercise1.cpp
@@ -10,16 +10,17 @@ using namespace std;
 class Person
 {
 private:
-	long int AM; //arithmos mitrwoy
-	string second_name;//
-	string name	; // onoma
-	string AN; //arithmos astinomikhs taftotitas
+	//PERSON Data
+	string first_name; // first_name
+	string last_name;//last name
+	long int AM; //ΑΡΙΘΜΟΣ ΜΗΤΡΟΟ
+	int age; // age of person
+	string doc_number; //arithmos astinomikhs taftotitas
 
 	static int person_counter;;//keeping track of object Persons created
-    /* data */
 
 public:
-    Person(long int x , string first_name , string last_name ,  string taftotita):AM(x),name(first_name),second_name(last_name) , AN(taftotita){
+    Person(string first_name , string last_name , int age , string AN , string doc_number):first_name(first_name),last_name(last_name), age(age) , AM(AM), doc_number(doc_number){
 		cout << "Person has been constructed" << endl;
 		person_counter++;
 	}; //initializer list
@@ -31,59 +32,75 @@ public:
 	void SetAM(long int arithmos){ //imutators
 		AM=arithmos;					
 	} 			
+	void SetAge(int age){
+		age = age;
+	}
 	void SetName(string first_name){	
-		name=first_name;			
+		first_name=first_name;			
 	}					
 	void SetSecondName(string last_name){
-		second_name=last_name;
+		last_name=last_name;
 	}
-	void SetAN(string taftotita){
-		AN=taftotita;
+	void SetAN(string doc_number){
+		doc_number=doc_number;
 	}
 	friend ostream & operator<<(ostream& par  ,const Person& pr) {
-		par << "Name: " << pr.name << endl << "Last Name: "<< pr.second_name << endl << "Arithmo mitro: " << pr.AM << endl << "Arithmo tautothtas: " << pr.AN << endl; 
+		par << "First Name: " << pr.first_name << endl << "Last Name: "<< pr.last_name << endl << "Age: " <<  pr.age << endl << "Arithmo mitro: " << pr.AM << endl << "Arithmo tautothtas: " << pr.doc_number << endl; 
 		return par;
 	}
-	friend istream & operator>>(istream& ins ,/*const not needed i think cause we change value of the pobject */ Person& det){
+	friend istream & operator>>(istream& ins , Person& det){
 		person_counter++; //because we create a new person without the constructor this means we should update the person counter
-		cout << "give Name" << endl;
-		ins  >> det.name;
-		cout << "give last Name:" << endl;
-		ins  >>  det.second_name; 
-		cout << "give AM :" << endl;
+		cout << "Please provide the following information about the person: " << endl;
+		cout << "First name: ";
+		ins  >> det.first_name;
+		cout << "Last Name: ";
+		ins  >>  det.last_name; 
+		cout << "Age: ";
+		ins >> det.age;
+		cout << "University identification number (should contain an 11 digit number):";
 		ins >> det.AM;
-		cout << "give AN :" << endl;
-		ins >> det.AN;
+		cout << "Document number: ";
+		ins >> det.doc_number;
 		return ins;
 	}
 
 	long int GetAM(){return AM;};
-	string GetFirstName(){return name;};
-	string GetLastName(){return second_name;};
-	string GetAN(){return AN;};
+	string GetFirstName(){return first_name;};
+	string GetLastName(){return last_name;};
+	int getAge(){return age;};
+	string GetAN(){return doc_number;};
+
 	~Person(){
 		cout << "Person has been destructed" << endl;
 	}
 };
 
 
+
 class Secretary
 {
 private:
 	map<long int, Person *> person_map ; // we gonna use map key:AM(aritmos mitrwou) value:person object
-	map<string , Person *> last_name_map;
+	// map<string , Person *> last_name_map;
 
 public:
 	Secretary(){}; //empty constructor
-	Secretary& operator+( Person& person){
-		Person * pr = new Person(person);  // dynamic allocation 
-		person_map[pr->GetAM()]=  pr; //add the person to the map	
-		return *this ;//return the objects pointer
+	Secretary& operator+(Person& person){
+			auto it = person_map.find(person.GetAM());
+			if (it == person_map.end()) {
+			Person* pr = new Person(person); // Dynamic allocation for new Person object
+			person_map[pr->GetAM()] = pr;   // Add the person to the map
+			} else {
+			// Person already exists, update its information
+			*(it->second) = person;
+    		}
+			return *this;
 	}
 	friend istream & operator>>(istream & input ,Secretary& sec){
-		Person person ; 
-		input >> person ;
-		sec=sec+person; //using the + operatot with the function above 
+		Person person; 
+		input >> person;
+		sec=sec+person; //using the + operatot with the function above
+
 		return input;
 	}
 	friend ostream& operator<<(ostream& output,const Secretary& sec){
@@ -96,29 +113,23 @@ public:
 	}
 
 	Secretary& operator=(const Secretary& sec){
-		if(this != &sec){
-				for(auto mp = person_map.begin(); mp != person_map.end(); mp++){
-
-				delete mp->second;
-			}
-			person_map.clear();
-
+		if (this != &sec){
+			for (const auto& person : person_map) {
+				delete person.second;
+    		}
 			for(auto mp = sec.person_map.begin(); mp != sec.person_map.end(); mp++){
-				Person* cloned = new Person(*(mp->second)); 
-
-				cout << "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!person map: " << cloned->GetAM();
-				person_map[mp->first] = cloned;
-
+			Person* cloned = new Person(*(mp->second)); 
+			person_map[mp->first] = cloned;
 			}			
-					}	
+		}
 		return *this;
 	}
 
 	Secretary(const Secretary& other):person_map(other.person_map){
-		for(auto mp = other.person_map.begin(); mp != other.person_map.end(); mp++){
-			Person* cloned = new Person(*(mp->second)); 
-			person_map[mp->first] = cloned;
-		}
+	 	for (const auto& person : other.person_map) {
+         Person* cloned = new Person(*(person.second));
+         person_map[person.first] = cloned;
+     	}
 	}; // copy constructor 
 
 	void ExistingAM(Person * person ){
@@ -134,13 +145,10 @@ public:
 		}		
 	}
 	~Secretary(){
-		for(auto pr = person_map.begin(); pr != person_map.end(); pr++){
-		  	delete pr->second;
-		}
-		auto pr =person_map.end();
-		delete pr->second;
-	
-	person_map.clear();
+		for (auto& person : person_map) {
+        	delete person.second;
+    	}
+    	person_map.clear();
 		cout << "secretary has been destructed" << endl;
 	}
 };
