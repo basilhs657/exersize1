7b63f89323463fcfdcc541d63f84f7e1deb75c80
basilhs657 <basihstzimpraku123@gmail.com>
2023-12-01 17:07:27 +0200

    changes

--- exercise1.cpp
+++ exercise1.cpp
@@ -15,16 +15,16 @@ private:
 	string last_name;//last name
 	// long int AM; //ΑΡΙΘΜΟΣ ΜΗΤΡΟΟ
 	int age; // age of person
-	string doc_number; //arithmos astinomikhs taftotitas
+	string doc_number; //arithmos astinomikhs taftotitas h diavatiriou
 	string email; // email
 
 	static int person_counter;;//keeping track of object Persons created
 
 public:
     Person(string first_name , string last_name , int age  , string doc_number , string email):first_name(first_name),last_name(last_name), age(age) , doc_number(doc_number) , email(email){
-	//cout << "Person has been constructed" << endl;
 		person_counter++;
 	}; //initializer list
+
 	Person(){}; //the constructor in to use the istream and ostream function 
 
 	void Person_counter(){
@@ -32,28 +32,27 @@ public:
     }
 
 	//SETTERS
-	// void SetAM(long int arithmos){ //imutators
-	// 	AM=arithmos;					
-	// } 			
 	void SetAge(int age){
-		age = age;
+		age = age; //setting the age
 	}
 	void SetName(string first_name){	
-		first_name=first_name;			
+		first_name=first_name;	//setting the first Name
 	}					
 	void SetSecondName(string last_name){
-		last_name=last_name;
+		last_name=last_name;  //setting the last name
 	}
 	void SetDoc_Num(string doc_number){
-		doc_number=doc_number;
+		doc_number=doc_number;  //setting the document number
 	}
 	void SetEmail(string email){
-		email = email;
+		email = email;	//setting the email
 	}
+	//creating the << operator overload that will return the person data as an ostream 
 	friend ostream & operator<<(ostream& par  ,const Person& pr) {
 		par << "First Name: " << pr.first_name << endl << "Last Name: "<< pr.last_name << endl << "Age: " <<  pr.age << endl << "email: " << pr.email << endl << "Arithmo tautothtas: " << pr.doc_number << endl; 
 		return par;
 	}
+	//creating the >> operator overload that will wait as input from the user and will set the data given the input the user gives 
 	friend istream & operator>>(istream& ins , Person& det){
 		person_counter++; //because we create a new person without the constructor this means we should update the person counter
 		cout << "Please provide the following information about the person: " << endl;
@@ -71,17 +70,14 @@ public:
 		return ins;
 	}
 
-	//MUTATORS
-	// long int GetAM(){return AM;};
+	//GETTERS
 	string GetFirstName(){return first_name;};
 	string GetLastName(){return last_name;};
 	int getAge(){return age;};
 	string GetDocNumber(){return doc_number;};
 	string GetEmail(){return email;};
-
-	~Person(){
-		// cout << "Person has been destructed" << endl;
-	}
+	// DESTRUCTOR
+	~Person(){}
 };
 
 
@@ -89,45 +85,63 @@ public:
 class Secretary
 {
 private:
-	map<string, Person *> person_map ; // we gonna use map key:document Number value:person object
-	map<string , Person *> extended_identificator; // finding a person with his first name and last name 
+	map<string, Person *> person_map ; //USING A MAP TO ASSIGHN A KEY (DOCUMENT VALUE) TO FIND A PERSON OBJECT
+	map<string , Person *> extended_identificator; // USING A MAP TO ASSIGHN A KEY(FIRST NAME , LAST NAME , EMAIL) TO FIND A PERSON OBJECT
 
 public:
 	Secretary(){}; //empty constructor
+	//creating the + operator overload
 	Secretary& operator+(Person& person){
-			Person* pr = new Person(person); 
-			person_map[pr->GetDocNumber()] = pr;
-			string first_last_name_email  = pr->GetFirstName() + pr->GetLastName() + pr->GetEmail();  
-			extended_identificator[first_last_name_email] = pr;
-			return *this;
+		//allocating memory to store a person in our two maps
+		Person* pr = new Person(person); 
+		//inserting a new pair with the key document number and  it's pair the pr (person object)
+		person_map[pr->GetDocNumber()] = pr;
+		//creating the string that will represent the key for our other map to find the person 
+		string first_last_name_email  = pr->GetFirstName() + pr->GetLastName() + pr->GetEmail();
+		//we use the same person that we allocatd memory before   
+		extended_identificator[first_last_name_email] = pr;
+		return *this;
 	}
+	//creating the >> operator overload
 	friend istream & operator>>(istream & input ,Secretary& sec){
+		//creating a new object
 		Person person; 
+		//using the >> overload function that we have created in the person object that will create and assighn values to a new person 
 		input >> person;
+		//trying to find if the data that was given as input corresponds to another person that already exists
 		auto per = sec.person_map.find(person.GetDocNumber());
+		//if the person does not exist which means the find dunction returned the map.end()
 		if(per == sec.person_map.end()){
-		  	sec=sec+person; //using the + operatot with the function above
+		  	sec=sec+person; //we will add the person to our secretary using the + fucntion above
 		}
 	 	else{
+			//if it already exists we print the following message
 		  	cout << "Person already exists!" << endl;
 		}
+		//we return the input
 		return input;
 	}
+	// creatin the << operator overload function
 	friend ostream& operator<<(ostream& output,const Secretary& sec){
 		cout << endl;
+		//we traverse the whole person_map to print all the person in our secretry class
 		for(auto itr =sec.person_map.begin() ; itr != sec.person_map.end() ; itr++){
-			output << *itr->second;	 //using the Persons class << operartor
+			output << *itr->second;	 //using the Persons class << operartor overload function
 			output << endl;
 		}
+		//we return the output
 		return output;
 	}
-
+	//creating the = operator overload function 
 	Secretary& operator=(const Secretary& sec){
+		//we check if ..
 		if (this != &sec){
 			for(auto mp = person_map.begin(); mp != person_map.end(); mp++){
+				//we delete the previus data
 				delete mp->second;
 			}			
 			for(auto mp = sec.person_map.begin(); mp != sec.person_map.end(); mp++){
+				//and we do a deep scan of the current data that we will clone to our assighned secretary 
 				Person* cloned = new Person(*(mp->second)); 
 				person_map[mp->first] = cloned;
 				string data = cloned->GetFirstName() + cloned->GetLastName() + cloned->GetEmail();
@@ -138,18 +152,20 @@ public:
 	}
 
 	Secretary(const Secretary& other):person_map(other.person_map){
+		//doing a deep scan since our secretary function works with dynamin memory allocation 
 	 	for (auto person = other.person_map.begin(); person != other.person_map.end(); person++){
     		Person* clone = new Person(*(person->second));
         	person_map[person->first] = clone;
      	}
 	}; // copy constructor 
 
+	//having access to all the different persons created 
 	void NumberOfPersons(const Secretary& other){
 		other.person_map.begin()->second->Person_counter();
 	}
-
+	//creatin a function that will get as input another person and using it's document number it will find if the person exists in the document class
 	void ExistingAM(Person * person){
-		if(person_map[person->GetDocNumber()]==NULL){ //the value of the map is a pointer 
+		if(person_map.find(person->GetDocNumber()) == person_map.end()){ //if the find fucntion returned map.end then we didnt find the person  
 			cout<<" PERSON DOES NOT EXIST"<< endl;
 		}
 		else
@@ -158,16 +174,20 @@ public:
 			cout << *person <<endl << endl; 
 		}		
 	}
+	//creating a function that will ask the user for the first and last name and email to find if the prson exists in our secretary class
 	void findUsingData(){
 		string name , last_name , email; 
+		cout << "Please pprovide the following information about the person you are searching: " << endl;
 		cout << "name of the person you are searcing: " << endl;
 		cin >> name; 
 		cout << "last name of the person you are seaching: " << endl;
 		cin >> last_name; 
 		cout <<"email of the person you are seacring: "<< endl;
 		cin >> email;
+		//we add the data to create an accurate key 
 		string data = name + last_name + email;
-		if(extended_identificator[data] == NULL){
+		//we check if the person exists
+		if(extended_identificator.find(data) == extended_identificator.end()){ 
 			cout << "Person does not exist!" << endl;
 		}
 		else{
@@ -175,10 +195,15 @@ public:
 			cout << *extended_identificator[data] << endl;  
 		}
 	}
+	//DESTRUCTORS 
 	~Secretary(){
+		//we delete all the dynamically allocated memory for the person objets we have created
 		for(auto mp = person_map.begin(); mp != person_map.end(); mp++){
 			delete mp->second;
 		}			
+		//since we used the same address for the object person in our person_map and extended_identificator map we don't have to delete the memory 
+		//for our extended_identificator map
+		//we delete the keys
     	person_map.clear();
 		extended_identificator.clear();
 		cout << "Secretary has been destructed" << endl << endl;
@@ -191,21 +216,34 @@ int Person::person_counter = 0;
 
 
 int main(){
-	//initializing a person 
-	Person p;
-	cin >> p;
-	//printing the number of persons created 
-	p.Person_counter();
+	//Checking the functionallity of secretary
+	//initializing an object of class secretary
 	Secretary secretary;
+	//inserting a new person to our secretary finction 
 	cin >> secretary;
+	//inserting another person to our secretary function 
 	cin >> secretary;
+	//finfing a person using his fisrt name , last name and email
 	secretary.findUsingData();
+	//initializing a new person 
 	Person find;
  	cin >> find;
+	//checking if the person exists
+	secretary.ExistingAM(&find);
+	//initializing a second secretary
  	Secretary replace;
+	//inserting the previus secretary to replace to check the = operator overload and the copy constructor s
  	replace = secretary;
+	//we add a new person to replace
+	cin >> replace;
+	//we try to find if a person exists in replace
 	replace.findUsingData();
- 	secretary.ExistingAM(&find);
- 	p.Person_counter();
+	//we output the number of persons created
 	replace.NumberOfPersons(replace);
+	//we pring all the persons of secretary 
+	cout << secretary;
+	//we print all the person of replace
+	cout << replace;
+
+	return 0;
 }
