3b9377e22f1ff7aab7220e8355aa062f46915ccd
Antonis-Sfikakis <AntonisSfikakis@gmail.com>
2024-01-08 18:16:02 +0200

    NIGGA

--- programs/secretary.h
+++ programs/secretary.h
@@ -1,4 +1,7 @@
 #include "person.h"
+#include "professor.h"
+#include "student.h"
+#include "course.h"
 
 using namespace std;
 
@@ -7,8 +10,13 @@ using namespace std;
 class Secretary
 {
     private:
-        map<string, Person *> person_map ; //USING A MAP TO ASSIGHN A KEY (DOCUMENT VALUE) TO FIND A PERSON OBJECT
-        map<string , Person *> extended_identificator; // USING A MAP TO ASSIGHN A KEY(FIRST NAME , LAST NAME , EMAIL) TO FIND A PERSON OBJECT
+        // map<string, Person *> person_map ; //USING A MAP TO ASSIGHN A KEY (DOCUMENT VALUE) TO FIND A PERSON OBJECT
+        // map<string , Person *> extended_identificator; // USING A MAP TO ASSIGHN A KEY(FIRST NAME , LAST NAME , EMAIL) TO FIND A PERSON OBJECT
+        map<Course *, Professor *> Course_Professor ;//key:courses  name , value :professor
+        map<Student * ,Course*>   Course_Sudent ;//each course student can join 
+        map<Professor * , pair<int ,Student *> > Professor_Student ; //each teacher gives a grade  for each student
+
+
 
     public:
         Secretary();
